d19e550565871ea8e386855645253de913a4555b
Umesh Suthar <umeshmankad555@gmail.com>
2025-09-16 18:20:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { createServer } from "./server";
+import { createServer } from "./server/index";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -36,3 +36,4 @@ function expressPlugin(): Plugin {
     },
   };
 }
+
